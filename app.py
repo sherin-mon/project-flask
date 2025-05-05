fc6b9fc0196192a9008459e0365c24d45b84f7cf
Sherin Mon Biju <sherinmonbiju@example.com>
2025-05-05 22:17:58 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, my name sherin mon biju i created a simple model application using python flasky"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
