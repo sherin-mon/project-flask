f47e32a2b7fe1751b568a7b6c216fde740fd8c5a
Sherin Mon Biju <sherinmonbiju@example.com>
2025-05-06 00:11:34 +0530

    Updated Flask app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, my name sherin mon biju i created a simple model application using python flask update"
+    return "Hello, my name sherin mon biju i created a simple model application using python flask update now"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
